3195458dc53938834ddfa54e230669b09cc65000
EdhikaaRGB <120156563+EdhikaaRGB@users.noreply.github.com>
2023-02-04 20:22:46 +0530

    Create mysql-new.sql

---
+++ datadump/mysql-new.sql
@@ -0,0 +1,4 @@
+INSERT INTO test.messages VALUES ('Devops');
+INSERT INTO test.messages VALUES ('Docker');
+INSERT INTO test.messages VALUES ('Pritam');
+INSERT INTO test.messages VALUES ('DevSecOps');
